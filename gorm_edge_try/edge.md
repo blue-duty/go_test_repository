42c703524dbeca01d5d3461a03653c1b147f1da7
blue-duty <lwduty666@gmail.com>
2022-08-29 00:34:27 +0800

    an attempt on edge in gorm

---
+++ gorm_edge_try/edge.md
@@ -0,0 +1,184 @@
+### 1. 对于一对一的关系的尝试
+初始化一张cxk表和一张chicken表，关联关系是一对一的关系，关联关系的类型是has_one，关联关系的方向是has_one的方向。
+``` go
+type Cxk struct {
+	Id      uint   `gorm:"primary_key"`
+	Name    string `gorm:"column:name"`
+	Chicken Chicken
+}
+
+func (Cxk) TableName() string {
+	return "cxk"
+}
+
+type Chicken struct {
+	Id    uint   `gorm:"primary_key"`
+	Name  string `gorm:"column:name"`
+	CxkId uint
+}
+
+func (Chicken) TableName() string {
+	return "chicken"
+}
+```
+跳过连接数据库的过程，我们初始化完成cxk表以及chicken表后，添加一只cxk的信息，并且添加一只chicken的信息，我们可以在添加cxk的时候直接为其绑定一只chicken。
+``` go
+    err = db.AutoMigrate(&Cxk{}, &Chicken{})
+    if err != nil {
+        return
+    }
+
+    db.Model(&Cxk{}).Create(&Cxk{
+        Id:   1,
+        Name: "cxk",
+        Chicken: Chicken{
+            Id:   1,
+            Name: "chicken",
+        },
+    })
+```
+这样我们就可以在查询cxk的时候直接查询出其绑定的chicken的信息。
+``` go
+var cxk Cxk
+db.First(&cxk)
+fmt.Printf("%#v", cxk)
+```
+使用上面的方法我们只能得到这样的结果
+``` go
+main.Cxk{Id:0x1, Name:"cxk", Chicken:main.Chicken{Id:0x0, Name:"", CxkId:0x0}}
+```
+我们只查询出cxk所对应的信息，却没有查出他拥有的chicken的信息，我们可以通过gorm的Preload方法来解决这个问题。
+``` go
+var cxk Cxk
+db.Preload("Chicken").First(&cxk)
+fmt.Printf("%#v", cxk)
+// 结果：main.Cxk{Id:0x1, Name:"cxk", Chicken:main.Chicken{Id:0x1, Name:"chicken", CxkId:0x1}}
+```
+### 2. 对于一对多的关系的尝试
+一只cxk肯定不止拥有一只chicken，我们可以通过has_many的方式来实现一对多的关系。
+``` go
+type Cxk struct {
+    Id      uint   `gorm:"primary_key"`
+    Name    string `gorm:"column:name"`
+    Chickens []Chicken
+}
+func (Cxk) TableName() string {
+    return "cxk"
+}
+type Chicken struct {
+    Id    uint   `gorm:"primary_key"`
+    Name  string `gorm:"column:name"`
+    CxkId uint
+}
+func (Chicken) TableName() string {
+    return "chicken"
+}
+```
+如法炮制，我们初始化了一张cxk表和一张chicken表，关联关系是一对多的关系，关联关系的类型是has_many，关联关系的方向是has_many的方向。并为表初始化了一组数据。
+``` go
+    err = db.AutoMigrate(&Cxk{}, &Chicken{})
+    if err != nil {
+        return
+    }
+    db.Model(&Cxk{}).Create(&Cxk{
+        Id:   1,
+        Name: "cxk",
+        Chickens: []Chicken{
+            {
+                Id:   1,
+                Name: "chicken1",
+            },
+            {
+                Id:   2,
+                Name: "chicken2",
+            },
+        },
+    })
+```
+这次我们不在添加cxk的时候直接为其绑定一只chicken，而是在添加chicken的时候直接为其绑定一个cxk。
+``` go
+    db.Model(&Chicken{}).Create(&Chicken{
+        Id:   3,
+        Name: "chicken3",
+        CxkId: 1,
+    })
+```
+这样我们就可以在查询cxk的时候直接查询出其绑定的chicken的信息。
+``` go
+var cxk Cxk
+db.Preload("Chickens").First(&cxk)
+fmt.Printf("%#v", cxk)
+// 结果：main.Cxk{Id:0x1, Name:"cxk", Chickens:[]main.Chicken{main.Chicken{Id:0x1, Name:"chicken1", CxkId:0x1}, main.Chicken{Id:0x2, Name:"chicken2", CxkId:0x1}, main.Chicken{Id:0x3, Name:"chicken3", CxkId:0x1}}}
+```
+### 3. 对于多对多的关系的尝试
+一只cxk肯定不止拥有一只chicken，一只chicken肯定不止属于一个cxk，我们可以通过many_to_many的方式来实现多对多的关系。
+``` go
+type Cxk struct {
+	Id       uint      `gorm:"primary_key"`
+	Name     string    `gorm:"column:name"`
+	Chickens []Chicken `gorm:"many2many:cxk_chicken;"`
+}
+
+func (Cxk) TableName() string {
+	return "cxk"
+}
+
+type Chicken struct {
+	Id   uint   `gorm:"primary_key"`
+	Name string `gorm:"column:name"`
+}
+
+func (Chicken) TableName() string {
+	return "chicken"
+}
+```
+此时将会生成一张cxk_chicken表，保存了cxk和chicken的关系，我们为其添加值。
+``` go
+    err = db.AutoMigrate(&Cxk{}, &Chicken{})
+    if err != nil {
+        return
+    }
+    db.Model(&Cxk{}).Create(&Cxk{
+        Id:   1,
+        Name: "cxk1",
+        Chickens: []Chicken{
+            {
+                Id:   1,
+                Name: "chicken1",
+            },
+            {
+                Id:   2,
+                Name: "chicken2",
+            },
+        },
+    })
+    db.Model(&Cxk{}).Create(&Cxk{
+        Id:   2,
+        Name: "cxk2",
+        Chickens: []Chicken{
+            {
+                Id:   1,
+                Name: "chicken1",
+            },
+            {
+                Id:   4,
+                Name: "chicken4",
+            },
+        },
+    })
+```
+此时cxk1拥有chicken1和chicken2，cxk2拥有chicken1和chicken4。
+查询cxk1以及chicken1的信息
+``` go
+// 查询cxk1的信息
+var cxk Cxk
+db.Preload("Chickens").Where("id = ?", 1).First(&cxk)
+fmt.Printf("%#v", cxk)
+// 结果：main.Cxk{Id:0x1, Name:"cxk", Chickens:[]main.Chicken{main.Chicken{Id:0x1, Name:"chicken1"}, main.Chicken{Id:0x2, Name:"chicken2"}}}
+
+// 查询chicken1的所有cxk的信息
+var cxks []Cxk
+db.Preload("Chickens", "id in (1)").Find(&cxks) //预加载的条件是id in (1)
+fmt.Printf("%#v", cxks)
+//结果：[]main.Cxk{main.Cxk{Id:0x1, Name:"cxk", Chickens:[]main.Chicken{main.Chicken{Id:0x1, Name:"chicken1"}}}, main.Cxk{Id:0x2, Name:"cxk2", Chickens:[]main.Chicken{main.Chicken{Id:0x1, Name:"chicken1"}}}}
+```
--- gorm_edge_try/main.go
+++ gorm_edge_try/main.go
@@ -4,171 +4,167 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 func main() {
-	// 创建一个新的ssh配置对象
-	//config := &ssh.ClientConfig{
-	// // 设置超时时间
-	// Timeout: time.Second * 10,
-	// // 设置ssh的私钥
-	// // PrivateKey: ssh.NewSignerFromKey(key),
-	// // 设置ssh的私钥的密码
-	// // Password: "123456",
-	// User:            "cos",
-	// Auth:            []ssh.AuthMethod{ssh.Password("we9621895")},
-	// HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	//}
-	//dialect, err := ssh.Dial("tcp", "192.168.28.180:22", config)
-	//if err != nil {
-	// fmt.Println("ssh.Dial error:", err)
-	// return
-	//}
-	//defer func(dialect *ssh.Client) {
-	// err := dialect.Close()
-	// if err != nil {
-	//  fmt.Println("dialect.Close error:", err)
-	// }
-	//}(dialect)
-	//session, err := dialect.NewSession()
-	//if err != nil {
-	// fmt.Println("dialect.NewSession error:", err)
-	// return
-	//}
-	//defer func(session *ssh.Session) {
-	// err := session.Close()
-	// if err != nil && err != io.EOF {
-	//  fmt.Println("session.Close error:", err)
-	// }
-	//}(session)
-	//// 运行命令
-	//// 读取命令的输出
-	//stdin, err := session.StdinPipe()
-	//if err != nil {
-	// log.Fatal("输入错误", err)
-	// return
-	//}
-	////设置session的标准输出和错误输出分别是os.stdout,os,stderr.就是输出到后台
-	////stdout, err := session.StdoutPipe()
-	//session.Stderr = os.Stderr
-	//session.Stdout = os.Stdout
-	//// 写入命令
-	//err = session.Start("passwd")
-	//if err != nil {
-	// return
-	//}
-	//time.Sleep(time.Second * 5)
-	////普通用户更改密码时需要输入旧密码
-	//_, err = fmt.Fprintf(stdin, "we9621895\n")
-	//if err != nil {
-	// log.Fatal("输入错误", err)
-	// return
-	//}
-	//time.Sleep(time.Second * 5)
-	//_, err = fmt.Fprintf(stdin, "trunkey123\n")
-	//if err != nil {
-	// log.Fatal("执行错误", err)
-	// return
-	//}
-	//time.Sleep(time.Second * 5)
-	//_, err = fmt.Fprintf(stdin, "trunkey123\n")
-	//if err != nil {
-	// log.Fatal("执行错误", err)
-	// return
-	//}
-	//time.Sleep(time.Second * 5)
-	//fmt.Println("执行完成")
-
-	//type test struct {
-	// name string
-	//}
-	//
-	//var t test
-	//t.name = "test"
-	//
-	//fmt.Println(t)
 	db, err := gorm.Open(mysql.Open("root:123456@tcp(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	//err = db.AutoMigrate(&Language{}, &User{})
-	//	//if err != nil {
-	//	//	return
-	//	//}
-	//	//db.Model(&Language{}).Create(&Language{
-	//	//	Name: "language" + strconv.Itoa(0),
-	//	//	Users: []User{
-	//	//		{
-	//	//			Name: "user" + strconv.Itoa(0),
-	//	//		},
-	//	//		{
-	//	//			Name: "user" + strconv.Itoa(1),
-	//	//		},
-	//	//	},
-	//	//})
 
-	//var c []User
-	//sql := "SELECT u.id,u.name,u.created_at FROM  users AS u JOIN user_languages AS ul ON u.id = ul.user_id WHERE ul.language_id = ?"
-	//db.Raw(sql, 1).Scan(&c)
-	//var c []User
-	var l Language
-	err = db.Preload(clause.Associations, "name not IN (?)", "user1").Find(&l).Error
+	err = db.AutoMigrate(&Cxk{}, &Chicken{})
 	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(l)
-	fmt.Println(l.Users)
-	for _, v := range l.Users {
-		fmt.Println(v.Name)
+		return
 	}
 
-}
+	db.Model(&Cxk{}).Create(&Cxk{
+		Id:   1,
+		Name: "cxk",
+		Chickens: []Chicken{
+			{
+				Id:   1,
+				Name: "chicken1",
+			},
+			{
+				Id:   2,
+				Name: "chicken2",
+			},
+		},
+	})
+	db.Model(&Cxk{}).Create(&Cxk{
+		Id:   2,
+		Name: "cxk2",
+		Chickens: []Chicken{
+			{
+				Id:   1,
+				Name: "chicken1",
+			},
+			{
+				Id:   4,
+				Name: "chicken4",
+			},
+		},
+	})
+	/*
+		db.Model(&Cxk{}).Create(&Cxk{
+			Id:   1,
+			Name: "cxk",
+			Chickens: []Chicken{
+				{
+					Id:   1,
+					Name: "chicken1",
+				},
+				{
+					Id:   2,
+					Name: "chicken2",
+				},
+			},
+		})
 
-type User struct {
-	Name string
-	gorm.Model
-	LanguageId uint
-}
+		db.Model(&Chicken{}).Create(&Chicken{
+			Id:    3,
+			Name:  "chicken3",
+			CxkId: 1,
+		})
 
-type Language struct {
-	gorm.Model
-	Name  string
-	Users []User
-}
+		db.Model(&Cxk{}).Create(&Cxk{
+			Id:   1,
+			Name: "cxk",
+			Chicken: Chicken{
+				Id:   1,
+				Name: "chicken",
+			},
+		})
 
-//type Test struct {
-// Name string json:"name"
-//}
+		db.Model(&Language{}).Create(&Language{
+			Name: "language" + strconv.Itoa(0),
+			Users: []User{
+				{
+					Name: "user" + strconv.Itoa(0),
+				},
+				{
+					Name: "user" + strconv.Itoa(1),
+				},
+			},
+		})
+
+		var c []User
+		sql := "SELECT u.id,u.name,u.created_at FROM  users AS u JOIN user_languages AS ul ON u.id = ul.user_id WHERE ul.language_id = ?"
+		db.Raw(sql, 1).Scan(&c)
+		var c []User
+		var l Language
+		err = db.Preload(clause.Associations, "name not IN (?)", "user1").Find(&l).Error
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(l)
+		fmt.Println(l.Users)
+		for _, v := range l.Users {
+			fmt.Println(v.Name)
+		}
+
+		var cxk Cxk
+		db.Preload("Chickens").Where("id = ?", 1).First(&cxk)
+		fmt.Printf("%#v", cxk)
+	*/
+	var cxks []Cxk
+	db.Preload("Chickens", "id in (1)").Find(&cxks)
+	fmt.Printf("%#v", cxks)
+}
 
 /*
-type User struct {
- Id   string gorm:"primary_key"
- Name string gorm:"column:name"
+type Cxk struct {
+	Id      uint   `gorm:"primary_key"`
+	Name    string `gorm:"column:name"`
+	Chicken Chicken
 }
-func (User) TableName() string {
- return "user"
-}
-
-type User2 struct {
- Id   string gorm:"primary_key"
- User User
+func (Cxk) TableName() string {
+	return "cxk"
 }
 
-func (User2) TableName() string {
- return "user2"
+type Chicken struct {
+	Id    uint   `gorm:"primary_key"`
+	Name  string `gorm:"column:name"`
+	CxkId uint
+}
+func (Chicken) TableName() string {
+	return "chicken"
 }
 
+type Cxk struct {
+	Id       uint   `gorm:"primary_key"`
+	Name     string `gorm:"column:name"`
+	Chickens []Chicken
+}
+func (Cxk) TableName() string {
+	return "cxk"
+}
 
-//type User struct {
-// gorm.Model
-// CreditCard []CreditCard
-//}
-//
-//type CreditCard struct {
-// gorm.Model
-// Number string
-// User   User `gorm:"ForeignKey:UserID"`
-//}
-//
+type Chicken struct {
+	Id    uint   `gorm:"primary_key"`
+	Name  string `gorm:"column:name"`
+	CxkId uint
+}
+func (Chicken) TableName() string {
+	return "chicken"
+}
 */
+
+type Cxk struct {
+	Id       uint      `gorm:"primary_key"`
+	Name     string    `gorm:"column:name"`
+	Chickens []Chicken `gorm:"many2many:cxk_chicken;"`
+}
+
+func (Cxk) TableName() string {
+	return "cxk"
+}
+
+type Chicken struct {
+	Id   uint   `gorm:"primary_key"`
+	Name string `gorm:"column:name"`
+}
+
+func (Chicken) TableName() string {
+	return "chicken"
+}
